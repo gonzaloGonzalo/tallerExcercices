1a4b11eb7d1b12c6834c16c60aa961b1ad5cc0b9
GJC <gonza_garay@hotmail.com>
2017-03-23 01:51:41 -0300

    Modification CountChar.

--- PracticalExcericise/src/Excercise3/CountChar.java
+++ PracticalExcericise/src/Excercise3/CountChar.java
@@ -17,5 +17,25 @@ public class CountChar {
                     Integer::sum);      // counting
         }
         System.out.println("Frequencies:\n" + freqs);
+
+        String str = "mmmmmeeeii";
+        int len = str.length();
+        Map<Character, Integer> numChars = new HashMap<Character, Integer>(Math.min(len, 26));
+
+        for (int i = 0; i < len; ++i)
+        {
+            char charAt = str.charAt(i);
+
+            if (!numChars.containsKey(charAt))
+            {
+                numChars.put(charAt, 1);
+            }
+            else
+            {
+                numChars.put(charAt, numChars.get(charAt) + 1);
+            }
+        }
+
+        System.out.println(numChars);
     }
 }
